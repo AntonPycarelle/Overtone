c463a5f74f45a3c82de4dc03ed9756a19448a56d
anton <anton_pycarelle@hotmail.com>
2020-09-04 14:06:53 +0200

    Final tweak-kunnrn

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -10,7 +10,7 @@ const About = ({ items }) => {
                 {/* <p className={styles.abouttext}>Ik hou van kaas</p> */}
                 <h2>Over Ons</h2>
                 <p className={styles.abouttext}>Wij zijn 2 studenten, Anton Pycarelle en Vic Debonne, die al enkele jaren <strong>ervaring</strong> hebben in de grafische sector. Wij studeren samen Digital Design & Development aan Howest Kortrijk en werken al 2 jaar samen in Overtone.
-            Na deze jaren ervaring is de <strong> grafische sector, webdesign </strong> en de hele online wereld meer geworden dan enkel een studie. Dag in dag uit zijn wij bezig met het grafische en daarom kunne wij U altijd <strong> het laatste nieuwe </strong> op grafisch vlak bieden.
+            Na deze jaren ervaring is de <strong> grafische sector, webdesign </strong> en de hele online wereld meer geworden dan enkel een studie. Dag in dag uit zijn wij bezig met het grafische en daarom kunnen wij U altijd <strong> het laatste nieuwe </strong> op grafisch vlak bieden.
 Een digitale of visuele oplossing nodig voor uw idee? Denk aan een <strong> website, marketing voor uw bedrijf, een poster, business cards, een gloednieuwe huisstijl,...</strong> Dan is Overtone een perfecte oplossing!</p>
 
                 <h2>Ons Team</h2>
